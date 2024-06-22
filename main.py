d947a320f8464fc553c711b17e0ed1a68483bc88
Duc Nguyen <ngmductm@gmail.com>
2024-06-22 23:13:42 +0700

    add some minor features and comments to the buttons and challenge command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bot import *
-from config import TOKEN, GUILD_ID
 from menus import *
+from config import TOKEN, GUILD_ID
 
 # Declare intents
 intents = discord.Intents.all()
@@ -12,18 +12,34 @@ bot = DuckshotBot("!", intents, GUILD_ID)
 @bot.tree.command()
 async def challenge(interaction: discord.Interaction, player: discord.Member):
     """Challenge another user in a gun fight with you"""
+    author = interaction.user  # The user who used the command
+
+    # You cannot challenge yourself or a bot
     if player.bot:
         await interaction.response.send_message("You cannot challenge a bot!")
-    elif player.id == interaction.user.id:
+    elif player.id == author.id:
         await interaction.response.send_message("Do you really want to kys? :face_with_raised_eyebrow: I don't think so")
-    else:
-        view = YesNoMenu(player)
+    else:  # Valid opponent
+        view = YesNoMenu(player)  # Buttons
         embed = discord.Embed(
             colour=discord.Colour.dark_purple(),
             title="Duckshot Challenge",
-            description=f"**{interaction.user.display_name}** challenged **{player.display_name}** in a gun fight!"
+            description=f"**{author.display_name}** challenged **{player.display_name}** in a gun fight!"
                         f"\n\nDo you accept this challenge?")
-        await interaction.response.send_message(embed=embed, view=view)
+        embed.set_author(name=author.display_name, icon_url=author.display_avatar.url)
+        await interaction.response.send_message(embed=embed, view=view)  # The display message
+
+        res = await view.wait()  # Check to see if the user reaches timeout
+
+        # Disable all buttons
+        for btn in view.children:
+            btn.disabled = True
+        await interaction.edit_original_response(view=view)
+
+        if res:  # User ran out of time
+            await interaction.followup.send("You ran out of time to decide. The challenge is cancelled!")
+        else:  # User accepted the challenge
+            pass
 
 # Run the bot
 bot.run(TOKEN)
--- menus.py
+++ menus.py
@@ -1,19 +1,22 @@
 import discord
-from discord.ext import commands
+
 
 class YesNoMenu(discord.ui.View):
     def __init__(self, opponent: discord.Member, timeout=60):
+        """Initialize the button menu with yes and no buttons"""
         super().__init__(timeout=timeout)
-        self.opponent = opponent
+        self.opponent = opponent  # The user that's being challenged
 
     @discord.ui.button(label="Yes", style=discord.ButtonStyle.green)
     async def yes(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if self.opponent.id == interaction.user.id:
-            await interaction.response.send_message("M gay")
+        """Yes button handling"""
+        if self.opponent.id == interaction.user.id:  # Only the opponent can decide
+            await interaction.response.send_message(f"**{interaction.user.display_name}** accepted the challenge!")
             self.stop()
 
     @discord.ui.button(label="No", style=discord.ButtonStyle.red)
     async def no(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if self.opponent.id == interaction.user.id:
-            await interaction.response.send_message(f"**{interaction.user.display_name}** declined the challenge")          
+        """No button handling"""
+        if self.opponent.id == interaction.user.id:  # Only the opponent can decide
+            await interaction.response.send_message(f"**{interaction.user.display_name}** declined the challenge!")
             self.stop()
