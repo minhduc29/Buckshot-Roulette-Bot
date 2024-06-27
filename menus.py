933d582a8d1476f7988f19caa62c12ecee8f8e3c
Duc Nguyen <ngmductm@gmail.com>
2024-06-27 08:51:10 +0700

    add the select menu for item usage in next game mode

--- menus.py
+++ menus.py
@@ -31,15 +31,20 @@ class YesNoMenu(discord.ui.View):
 
 
 class ShootMenu(discord.ui.View):
-    def __init__(self, current_player_id):
+    def __init__(self, player_id, item_info=None, player_item=None):
         super().__init__()
-        self.id = current_player_id
+        self.player_id = player_id
         self.shot_yourself = None
+        self.selected = None
+        if item_info and player_item:
+            item_menu = ItemMenu(item_info, player_item, player_id, self)
+            if item_menu.options:  # Add select menu if player has items to use
+                self.add_item(item_menu)
 
     @discord.ui.button(label="Shoot yourself", style=discord.ButtonStyle.primary)
     async def shoot_yourself(self, interaction: discord.Interaction, button: discord.ui.Button):
         """Handle shoot yourself action"""
-        if interaction.user.id == self.id:
+        if interaction.user.id == self.player_id:
             await interaction.response.defer()
             self.shot_yourself = True
             self.stop()
@@ -47,7 +52,7 @@ class ShootMenu(discord.ui.View):
     @discord.ui.button(label="Shoot opponent", style=discord.ButtonStyle.red)
     async def shoot_opponent(self, interaction: discord.Interaction, button: discord.ui.Button):
         """Handle shoot your opponent action"""
-        if interaction.user.id == self.id:
+        if interaction.user.id == self.player_id:
             await interaction.response.defer()
             self.shot_yourself = False
             self.stop()
@@ -56,3 +61,26 @@ class ShootMenu(discord.ui.View):
         """Disable all buttons"""
         for button in self.children:
             button.disabled = True
+
+
+class ItemMenu(discord.ui.Select):
+    def __init__(self, item_info, player_item, player_id, view: discord.ui.View):
+        super().__init__(placeholder="Choose an item to use")
+        self.item_info = item_info  # Description of items
+        self.player_item = player_item  # Items that the player have
+        self.player_id = player_id  # The player that can select the item
+        self.parent = view  # The parent view component above this select menu
+        self.add_items()  # Add all player items as options to this select menu
+
+    def add_items(self):
+        """Add player items as options to this select menu"""
+        for item in self.item_info:
+            if self.player_item[item]:  # If player has this item
+                self.add_option(label=item, description=self.item_info[item])
+
+    async def callback(self, interaction: discord.Interaction):
+        """Event handler when the player selects an item to use"""
+        if interaction.user.id == self.player_id:
+            await interaction.response.defer()
+            self.parent.selected = self.values[0]
+            self.parent.stop()
